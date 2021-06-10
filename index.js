f39a9e3b14f6009e8598027d1d0089eae5445198
ponnarasi <>
2021-06-10 22:03:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//let phrase = 'Code for fun';
+//console.log(phrase[2]);
+
+let myStr = 'Index';
+console.log(myStr[2] === 'n');
+console.log(myStr[4] === 'x');
+console.log(myStr[6] === ' ');
+console.log(myStr[0] === 'I');
+
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+
+let firstName = "Grace";
+let lastName = "Hopper";
+
+console.log(firstName, "has", firstName.length, "characters");
+console.log(lastName, "has", lastName.length, "characters");
+
+//let nonprofit = "LaunchCode"; 
+
+
+//console.log(nonprofit.toLowerCase());
+//console.log(nonprofit);
+
+
+
+
+let nonprofit = "Launchcode";
+
+console.log(nonprofit);
+nonprofit[6] = "C";
+console.log(nonprofit);
+
+let pet = 'cat';
+console.log(pet + 's');
+console.log(pet += 's');
